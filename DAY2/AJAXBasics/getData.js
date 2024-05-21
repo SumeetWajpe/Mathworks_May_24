720f83a81789931c76d868a9a924a5136c73d3c5
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-05-21 17:08:47 +0530

    ✨ Using Callback function to pass the data

--- DAY2/AJAXBasics/getData.js
+++ DAY2/AJAXBasics/getData.js
@@ -1,12 +1,12 @@
-function GetData() {
+function GetData(callback) {
   // AJAX request to fetch the data
   let xmlHttpReq = new XMLHttpRequest();
-  xmlHttpReq.open("GET", "https://jsonplaceholder.typicode.com/postsss"); // config settings
+  xmlHttpReq.open("GET", "https://jsonplaceholder.typicode.com/posts"); // config settings
 
   // when ready state changes 0 -> 1 -> 2 - > 3 -> 4
   xmlHttpReq.onreadystatechange = function () {
     if (xmlHttpReq.readyState == 4 && xmlHttpReq.status == 200) {
-      console.log(xmlHttpReq.responseText);
+      callback(xmlHttpReq.responseText);
     } else if (xmlHttpReq.readyState == 4 && xmlHttpReq.status !== 200) {
       console.log("Something went wrong !", xmlHttpReq.status);
     }
