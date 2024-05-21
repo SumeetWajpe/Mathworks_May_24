888c40fc19183ed1f044bba066f43e62b9bc9598
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-05-21 11:00:29 +0530

    ✨ ES6 Modules

---
+++ DAY1/Functions/Calc.js
@@ -0,0 +1,1 @@
+console.log(MathModule.Add(10, 20));
---
+++ DAY2/ES6/Modules/calc.js
@@ -0,0 +1,3 @@
+import { Add, Product as Multiply } from "./math.js";
+console.log(`The addition is : ${Add(20, 30)}`);
+console.log(`The multiplication is : ${Multiply(20, 30)}`);
