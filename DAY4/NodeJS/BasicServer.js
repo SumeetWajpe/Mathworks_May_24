a66f0739a35571a72476356c2c3c1802b7b9a8fd
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-05-23 15:29:28 +0530

    ✨ Serving html page using fs module

--- DAY4/NodeJS/BasicServer.js
+++ DAY4/NodeJS/BasicServer.js
@@ -1,13 +1,19 @@
 var http = require("http");
+var fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World!\n");
+  fs.readFile("Index.html", (err, data) => {
+    if (data) {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    } else {
+      res.writeHead(500, { "Content-Type": "text/html" });
+      res.end("Something went wrong !");
+    }
+  });
 });
 
 // starts a simple http server locally on port 3000
 server.listen(3000, "127.0.0.1", () => {
   console.log("Listening on 127.0.0.1:3000");
 });
-
-
